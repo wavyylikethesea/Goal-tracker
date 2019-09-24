96c7a7de902d79766c83336832e0cd3b426f32b1
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-24 12:14:17 +0300

    Create goal class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Goal } from './goals';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'goals';
+ goals: Goal[] = [
+   { id: 1, name: 'Watch finding Nemo' },
+   { id: 2, name: 'Buy Cookies' },
+   { id: 3, name: 'Get new Phone Case' },
+   { id: 4, name: 'Get Dog Food' },
+   { id: 5, name: 'Solve math homework' },
+   { id: 6, name: 'Plot my world domination plan' },
+ ]
 }
