53e261f740ad5da2e57abf625395a59c71cd8113
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-26 11:34:59 +0300

    Get rid of the goals array for the service to inject the goals

--- src/app/goal-service/goal.service.ts
+++ src/app/goal-service/goal.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Goal } from '../goals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalService {
+
+  getGoals(){
+    return Goal
+  }
 
   constructor() { }
 }
