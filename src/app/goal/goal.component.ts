28c467010023edbd7208553e3ddfde98848cb268
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-26 10:50:51 +0300

    Add goal submit button function

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -32,6 +32,13 @@ export class GoalComponent implements OnInit {
       }
     }
   }
+
+  addNewGoal(goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
   
   constructor() { }
 
